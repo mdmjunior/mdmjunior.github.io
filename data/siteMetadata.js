39ca371da6c5aef63e3ed42f95243ac494726725
Marcio Moreira Junior <marcio.moreira@mobi2buy.com>
2025-02-08 17:12:47 -0300

    dsadsadas

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'My personal tech blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://mdmjunior.github.io',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
@@ -20,11 +20,11 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/mdmjunior',
   //threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com/marciomjunior',
-  //medium: 'https://medium.com',
+  medium: 'https://medium.com/mdmjunior',
   //bluesky: 'https://bsky.app/',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
-  stickyNav: false,
+  stickyNav: true,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
