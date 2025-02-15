e4aa5baa61af46c9cd83f8459d8d2eadd45ce62c
Marcio Moreira Junior <mdmjunior@gmail.com>
2025-02-15 19:14:03 -0300

    Aprimora o estilo do layout do autor: torna o nome em negrito e remove o negrito da empresa

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -30,9 +30,9 @@ export default function AuthorLayout({ children, content }: Props) {
                 className="h-48 w-48 rounded-full"
               />
             )}
-            <h3 className="pt-4 pb-2 text-2xl leading-8 tracking-tight">{name}</h3>
+            <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400 font-bold">{company}</div>
+            <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
