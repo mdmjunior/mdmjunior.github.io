802c046ee598527289d0fa42c2815693916f0b37
Marcio Moreira Junior <mdmjunior@gmail.com>
2025-02-17 23:37:19 -0300

    Remove imagens desnecessárias da pasta estática

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,12 +12,6 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/AutoM8.png',
     href: 'https://www.github.com/mdmjunior/AutoM8/',
   },
-  {
-    title: 'Shell',
-    description: `Under Development`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
 ]
 
 export default projectsData
