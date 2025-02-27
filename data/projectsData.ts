b92163f9963ceddaa6c3b2bd613ddb31228d8089
Marcio Moreira Junior <mdmjunior@gmail.com>
2025-02-27 18:14:19 -0300

    Atualiza descrições dos projetos AutoM8 e BConverter

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,7 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'AutoM8 - Linux Ubuntu Post-Install Tool',
-    description: `Under Development`,
+    description: `A set of shell scripts to automate the process of installing software on Ubuntu Desktop`,
     imgSrc: '/static/images/AutoM8.png',
     href: 'https://github.com/mdmjunior/AutoM8',
     repo: 'https://github.com/mdmjunior/AutoM8',
@@ -17,7 +17,7 @@ const projectsData: Project[] = [
 
   {
     title: 'BConverter - Bash script to create Markdown Documents',
-    description: `My bash tool to conver a simple text file into a markdown document`,
+    description: `My bash tool to convert a simple text file into a markdown document`,
     imgSrc: '/static/images/bconverter.jpeg',
     href: 'https://github.com/mdmjunior/BConverter',
     repo: 'https://github.com/mdmjunior/BConverter',
