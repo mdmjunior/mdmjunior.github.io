6181616cf5ae0ccecd0693b3f336d0d71a28a241
Marcio Moreira Junior <118519+mdmjunior@users.noreply.github.com>
2026-04-30 23:42:14 -0300

    Simplify footer social links and credit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,15 +9,7 @@ export default function Footer() {
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={5} />
           <SocialIcon kind="github" href={siteMetadata.github} size={5} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={5} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={5} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={5} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={5} />
-          <SocialIcon kind="bluesky" href={siteMetadata.bluesky} size={5} />
-          <SocialIcon kind="x" href={siteMetadata.x} size={5} />
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={5} />
-          <SocialIcon kind="threads" href={siteMetadata.threads} size={5} />
-          <SocialIcon kind="medium" href={siteMetadata.medium} size={5} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.title}</div>
@@ -25,8 +17,8 @@ export default function Footer() {
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Powered by Tailwind Nextjs
+          <Link href="https://github.com/mdmjunior/mdmjunior.github.io">
+            Experiment using Tailwind Next.js
           </Link>
         </div>
       </div>
